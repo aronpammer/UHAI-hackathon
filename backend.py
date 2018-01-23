30fe66c8cb6966728508769e6193f4daba281f56
Aron Pammer <aron.pammer@skyscanner.net>
2018-01-23 20:10:40 +0100

    added history retrieval

--- backend.py
+++ backend.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, redirect
 import blockchain
 # Instantiate the Node
 app = Flask(__name__)
@@ -22,9 +22,13 @@ def history():
         # get file_url from ipfs
         file_url = response["fileHash"]
         result = response["diagnosis"]
+        date_time = response["date"]
+        description = response["description"]
         diagnosises.append({
             "file_url": file_url,
-            "result": result
+            "description": description,
+            "result": float(result) * 100,
+            "date": date_time
         })
     return render_template("history.html", diagnosises=diagnosises)
 
@@ -36,30 +40,35 @@ def upload_image():
         if 'image-file' not in request.files:
             return 'No file part'
         file = request.files['image-file']
-        upload_file_to_ipfs_blockchain(file)
-    return "file uploaded"
+        description = request.form['description']
+        upload_file_to_ipfs_blockchain(file, description)
+    return redirect("/history", code=302)
 
 # there can be any number of contract addresses, ideally one per patient, for the demo we are only using one
-STATIC_CONTRACT_ADDRESS = "0x6477e21f70ee303bb0a47b96a737cbf64eb99852"
+STATIC_CONTRACT_ADDRESS = "0xe12fe06c6e5b3d94bd7a2178fda3f8029b9f0361"
 # the from address would be ideally the service's address that uploads the data to the blockchain, in this case UHAI
 STATIC_FROM_ADDRESS = "0x0fC1A83F77FA3C9f53dbA8B439D861faA35fE315"
 
-def upload_file_to_ipfs_blockchain(file):
+def upload_file_to_ipfs_blockchain(file, description):
     file_hash = "FILEHASH"  #IpfsInterface.addFileObj(file)
     print("file hash {file}".format(file=file_hash))
     # here we are going to analyze the data
     # ...
-    result = "0.9"
+    result = "0.3"
 
     # here we are going to upload the data to eth
 
     import requests
-    response = requests.get("http://localhost:1337/add_diagnosis/{contract_address}?filehash={file_hash}&result={result}&from={from_address}&gas={gas}".format(
+    import datetime
+    date_time = datetime.datetime.today().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    response = requests.get("http://localhost:1337/add_diagnosis/{contract_address}?filehash={file_hash}&result={result}&from={from_address}&gas={gas}&description={description}&datetime={datetime}".format(
         contract_address=STATIC_CONTRACT_ADDRESS,
         file_hash=file_hash,
         result=result,
+        description=description,
         from_address=STATIC_FROM_ADDRESS,
-        gas=3000000
+        gas=3000000,
+        datetime=date_time
     )).text
     print(response)
 
@@ -67,10 +76,18 @@ def get_diagnosis_from_blockchain():
     import requests
     id = 0
     while True:
-        response = requests.get("http://localhost:1337/diagnosis/{contract_address}/{id}".format(
-            contract_address=STATIC_CONTRACT_ADDRESS,
-            id=id
-        )).text
+        for i in range(5):
+            # don't look at this please, waaaay too hacky...
+            try:
+                response = requests.get("http://localhost:1337/diagnosis/{contract_address}/{id}".format(
+                    contract_address=STATIC_CONTRACT_ADDRESS,
+                    id=id
+                ), timeout=2.5).text
+                break
+            except:
+                pass
+            if i == 4:
+                raise "Load error"
         print(response)
         if "ERROR" in response:
             break
@@ -89,7 +106,7 @@ if __name__ == '__main__':
     from argparse import ArgumentParser
 
     parser = ArgumentParser()
-    parser.add_argument('-p', '--port', default=8888, type=int, help='port to listen on')
+    parser.add_argument('-p', '--port', default=8000, type=int, help='port to listen on')
     args = parser.parse_args()
     port = args.port
 
--- web3js/web3.js
+++ web3js/web3.js
@@ -1,22 +1,36 @@
-var Web3 = require('web3');
+/*
+* you need the newest web3js to run this, 1.0 (npm install https://github.com/ethereum/web3.js#v1.0.0-beta.17)
+* you need express to run the web service (npm install -g express)
 
-var web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io'));
+steps to fill in the variables:
+- go on http://remix.ethereum.org/
+- use for example this contract:
+contract PatientInfo {
+    struct diagnosisInfo {
+        string fileHash;
+        string diagnosis;
+        string description;
+    }
+
+    diagnosisInfo[] public diagnosises;
+
+    function addDiagnosis(string fileHash, string diagnosis, string description) {
+        diagnosises.push(diagnosisInfo(fileHash, diagnosis, description));
+    }
+    function getDiagnosesCount() public constant returns(uint count) {
+        return diagnosises.length;
+    }
+}
+
+- go on Run, then compile it on the ropsten network (play network), or any network really
+- create a wallet, put some ether on it, and fill in `my_private_key`
+- copy contractJson from http://remix.ethereum.org/ Compile tab, Details button, copy ABI
+- change contract address in backend.py
+*/
+
+var my_private_key = "0x1e10130053d9528dd059706a8e8ff7cdea373c4b3437f9b0e66d8d68bcf6b528"
 
 var contractJson = [
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "patientId",
-		"outputs": [
-			{
-				"name": "",
-				"type": "string"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
 	{
 		"constant": true,
 		"inputs": [],
@@ -48,6 +62,14 @@ var contractJson = [
 			{
 				"name": "diagnosis",
 				"type": "string"
+			},
+			{
+				"name": "description",
+				"type": "string"
+			},
+			{
+				"name": "date",
+				"type": "string"
 			}
 		],
 		"payable": false,
@@ -64,6 +86,14 @@ var contractJson = [
 			{
 				"name": "diagnosis",
 				"type": "string"
+			},
+			{
+				"name": "description",
+				"type": "string"
+			},
+			{
+				"name": "date",
+				"type": "string"
 			}
 		],
 		"name": "addDiagnosis",
@@ -71,24 +101,12 @@ var contractJson = [
 		"payable": false,
 		"stateMutability": "nonpayable",
 		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"name": "patientId",
-				"type": "string"
-			}
-		],
-		"name": "setPatientInfo",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
 	}
 ];
 
-var my_private_key = "0x1e10130053d9528dd059706a8e8ff7cdea373c4b3437f9b0e66d8d68bcf6b528"
+var Web3 = require('web3');
+
+var web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io'));
 web3.eth.accounts.wallet.add(my_private_key);
 
 
@@ -101,6 +119,7 @@ app.get('/', (request, response) => {
 });
 
 app.get('/diagnosis/:contractaddress/:id', (request, response) => {
+  console.log("processing get request...");
   let id = request.params.id;
   //0x6477e21f70ee303bb0a47b96a737cbf64eb99852
   var contractAddress = request.params.contractaddress;
@@ -118,18 +137,20 @@ app.get('/diagnosis/:contractaddress/:id', (request, response) => {
 });
 
 
-//http://localhost:1337/add_diagnosis/0xccf510c7e770bce34345fc519795be9b6eee41cb?filehash=text1&result=test2&from=0x0fC1A83F77FA3C9f53dbA8B439D861faA35fE315&gas=3000000
+//http://localhost:1337/add_diagnosis/0xccf510c7e770bce34345fc519795be9b6eee41cb?filehash=text1&result=test2&from=0x0fC1A83F77FA3C9f53dbA8B439D861faA35fE315&gas=3000000&datetime=today
 app.get('/add_diagnosis/:contractaddress', (request, response) => {
   let contractAddress = request.params.contractaddress;
   let fileHash = request.query.filehash;
   let result = request.query.result;
+  let description = request.query.description;
   //0x0fC1A83F77FA3C9f53dbA8B439D861faA35fE315
   let fromAddress = request.query.from;
   //3000000
   let gas = request.query.gas;
+  let datetime = request.query.datetime;
 
   var contract = new web3.eth.Contract(contractJson, contractAddress);
-  var result2 = contract.methods.addDiagnosis(fileHash, result).send({from: fromAddress, gas: gas}, function(error, result) {
+  var result2 = contract.methods.addDiagnosis(fileHash, result, description, datetime).send({from: fromAddress, gas: gas}, function(error, result) {
         console.log(result);
         console.log(error);
 
@@ -148,14 +169,3 @@ app.listen(port, (err) => {
   console.log(`server is listening on ${port}`)
 });
 
-
-/*
-var result = contract.methods.diagnosises(1).call(function(error, result) {
-    console.log(result);
-    console.log(error);
-});
-
-var result2 = contract.methods.addDiagnosis("aaa2", "bbb2").send({from: "0x0fC1A83F77FA3C9f53dbA8B439D861faA35fE315", gas: "3000000"}, function(error, result) {
-    console.log(result);
-    console.log(error);
-});*/
