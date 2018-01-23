4184bb775556921b34b6c0c4c1326d2d42e722fd
Aron Pammer <aron.pammer@skyscanner.net>
2018-01-23 18:20:08 +0100

    added better backend and better web3

--- backend.py
+++ backend.py
@@ -16,7 +16,17 @@ def upload():
 
 @app.route('/history', methods=['GET'])
 def history():
-    return render_template("history.html")
+    diagnosises = []
+    for response in get_diagnosis_from_blockchain():
+        print(response)
+        # get file_url from ipfs
+        file_url = response["fileHash"]
+        result = response["diagnosis"]
+        diagnosises.append({
+            "file_url": file_url,
+            "result": result
+        })
+    return render_template("history.html", diagnosises=diagnosises)
 
 
 @app.route('/upload_image', methods=['POST'])
@@ -26,18 +36,47 @@ def upload_image():
         if 'image-file' not in request.files:
             return 'No file part'
         file = request.files['image-file']
-        blockchain.uploadFile(file)
+        upload_file_to_ipfs_blockchain(file)
     return "file uploaded"
 
-def upload_file_to_ipfs(file):
-    import IpfsInterface
-    file_hash = IpfsInterface.addFileObj(file)
+# there can be any number of contract addresses, ideally one per patient, for the demo we are only using one
+STATIC_CONTRACT_ADDRESS = "0x6477e21f70ee303bb0a47b96a737cbf64eb99852"
+# the from address would be ideally the service's address that uploads the data to the blockchain, in this case UHAI
+STATIC_FROM_ADDRESS = "0x0fC1A83F77FA3C9f53dbA8B439D861faA35fE315"
+
+def upload_file_to_ipfs_blockchain(file):
+    file_hash = "FILEHASH"  #IpfsInterface.addFileObj(file)
+    print("file hash {file}".format(file=file_hash))
     # here we are going to analyze the data
     # ...
+    result = "0.9"
 
     # here we are going to upload the data to eth
-    # ...
-    print("file hash {file}".format(file=file_hash))
+
+    import requests
+    response = requests.get("http://localhost:1337/add_diagnosis/{contract_address}?filehash={file_hash}&result={result}&from={from_address}&gas={gas}".format(
+        contract_address=STATIC_CONTRACT_ADDRESS,
+        file_hash=file_hash,
+        result=result,
+        from_address=STATIC_FROM_ADDRESS,
+        gas=3000000
+    )).text
+    print(response)
+
+def get_diagnosis_from_blockchain():
+    import requests
+    id = 0
+    while True:
+        response = requests.get("http://localhost:1337/diagnosis/{contract_address}/{id}".format(
+            contract_address=STATIC_CONTRACT_ADDRESS,
+            id=id
+        )).text
+        print(response)
+        if "ERROR" in response:
+            break
+        import json
+        yield json.loads(response)
+        id += 1
 
 
 
--- web3js/web3.js
+++ web3js/web3.js
@@ -19,6 +19,20 @@ var contractJson = [
 	},
 	{
 		"constant": true,
+		"inputs": [],
+		"name": "getDiagnosesCount",
+		"outputs": [
+			{
+				"name": "count",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
 		"inputs": [
 			{
 				"name": "",
@@ -74,9 +88,6 @@ var contractJson = [
 	}
 ];
 
-var contractAddress = "0xccf510c7e770bce34345fc519795be9b6eee41cb";
-var contract = new web3.eth.Contract(contractJson, contractAddress);
-
 var my_private_key = "0x1e10130053d9528dd059706a8e8ff7cdea373c4b3437f9b0e66d8d68bcf6b528"
 web3.eth.accounts.wallet.add(my_private_key);
 
@@ -89,14 +100,45 @@ app.get('/', (request, response) => {
   response.send('Hello from Express!')
 });
 
-app.get('/diagnosis/:id', (request, response) => {
+app.get('/diagnosis/:contractaddress/:id', (request, response) => {
   let id = request.params.id;
-
+  //0x6477e21f70ee303bb0a47b96a737cbf64eb99852
+  var contractAddress = request.params.contractaddress;
+  var contract = new web3.eth.Contract(contractJson, contractAddress);
   contract.methods.diagnosises(id).call(function(error, result) {
         console.log(result);
         console.log(error);
-        response.send(result);
+
+        if(result != null) {
+            response.send(result);
+        } else {
+            response.send(error);
+        }
     });
+});
+
+
+//http://localhost:1337/add_diagnosis/0xccf510c7e770bce34345fc519795be9b6eee41cb?filehash=text1&result=test2&from=0x0fC1A83F77FA3C9f53dbA8B439D861faA35fE315&gas=3000000
+app.get('/add_diagnosis/:contractaddress', (request, response) => {
+  let contractAddress = request.params.contractaddress;
+  let fileHash = request.query.filehash;
+  let result = request.query.result;
+  //0x0fC1A83F77FA3C9f53dbA8B439D861faA35fE315
+  let fromAddress = request.query.from;
+  //3000000
+  let gas = request.query.gas;
+
+  var contract = new web3.eth.Contract(contractJson, contractAddress);
+  var result2 = contract.methods.addDiagnosis(fileHash, result).send({from: fromAddress, gas: gas}, function(error, result) {
+        console.log(result);
+        console.log(error);
+
+        if(result != null) {
+            response.send("transaction queued: " + result);
+        } else {
+            response.send(error);
+        }
+  });
 });
 
 app.listen(port, (err) => {
